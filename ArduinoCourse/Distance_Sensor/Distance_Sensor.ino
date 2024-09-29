d95380f922a435869a36801ac1b316e7c8e738ff
Teemu Harinen <teemuharinen1@gmail.com>
2024-09-29 21:02:18 +0300

    add led lights based off distance

--- ArduinoCourse/Distance_Sensor/Distance_Sensor.ino
+++ ArduinoCourse/Distance_Sensor/Distance_Sensor.ino
@@ -1,8 +1,15 @@
 #define ECHO_PIN 3
 #define TRIGGER_PIN 4
+#define LED_GREEN 10
+#define LED_YELLOW 11
+#define LED_RED 12
 
 unsigned long lastTimeUltrasonicTrigger = millis();
 unsigned long ultrasonicTriggerDelay = 100;
+
+volatile unsigned long pulseInTimeBegin;
+volatile unsigned long pulseInTimeEnd;
+volatile bool newDistanceAvailable = false;
 
 void triggerUltrasonicSensor()
 {
@@ -15,9 +22,44 @@ void triggerUltrasonicSensor()
 
 double getUltrasonicDistance()
 {
-  double durationMicros = pulseIn(ECHO_PIN, HIGH);
+  double durationMicros = pulseInTimeEnd - pulseInTimeBegin;
   double distance = durationMicros / 58.0; //  -> cm
   return distance;
+}
+
+void echoPinInterrupt()
+{
+  if (digitalRead(ECHO_PIN) == HIGH)
+  {
+    pulseInTimeBegin = micros();
+  }
+  else
+  {
+    pulseInTimeEnd = micros();
+    newDistanceAvailable = true;
+  }
+}
+
+void powerLEDs(double distance)
+{
+    if (distance > 100)
+    {
+      digitalWrite(LED_GREEN, HIGH);
+      digitalWrite(LED_YELLOW, LOW);
+      digitalWrite(LED_RED, LOW);
+    }
+    else if (distance > 15)
+    {
+      digitalWrite(LED_GREEN, LOW);
+      digitalWrite(LED_YELLOW, HIGH);
+      digitalWrite(LED_RED, LOW);
+    }
+    else
+    {
+      digitalWrite(LED_GREEN, LOW);
+      digitalWrite(LED_YELLOW, LOW);
+      digitalWrite(LED_RED, HIGH);
+    }
 }
 
 
@@ -25,6 +67,14 @@ void setup() {
   Serial.begin(115200);
   pinMode(ECHO_PIN, INPUT);
   pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_YELLOW, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+
+  attachInterrupt(digitalPinToInterrupt(ECHO_PIN),
+                  echoPinInterrupt,
+                  CHANGE);
+  
 }
 
 void loop() {
@@ -34,6 +84,14 @@ void loop() {
   {
     lastTimeUltrasonicTrigger += ultrasonicTriggerDelay;
     triggerUltrasonicSensor();
-    Serial.println(getUltrasonicDistance());
+  }
+
+  if (newDistanceAvailable)
+  {
+    newDistanceAvailable = false;
+    double distance = getUltrasonicDistance();
+    Serial.println(distance);
+    
+    powerLEDs(distance);
   }
 }
